8183c7b5b1992f71ab564585d5bd25254a31bd7c
shreetenguria-cpu <shreetenguria@gmail.com>
2026-01-14 03:35:35 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = "menu-v1";
+const ASSETS = ["./","index.html","logo.png"];
+
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  );
+});
